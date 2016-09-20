60adba3a6a23fa3f50d445892a9d0b1c980f3b1c
HPEbot <hpsauth@gmail.com>
2016-09-20 13:19:10 +0300

    Update HelloWorldBuilder.java

--- src/main/java/hudson/plugins/hello_world/HelloWorldBuilder.java
+++ src/main/java/hudson/plugins/hello_world/HelloWorldBuilder.java
@@ -123,6 +123,8 @@ public class HelloWorldBuilder extends Builder {
         public boolean useRFrenchForSonarTesting() {
             int French;
             French = 1;
+            French = 2;
+            String c;
             return false;
         }
         
